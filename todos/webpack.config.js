8e4e5021db6c40739985dfabe42337930d07f65f
CalvinJin <jinchi2013us@gmail.com>
2016-07-25 14:40:48 -0400

    add .babelrc and done with webpack config

--- todos/webpack.config.js
+++ todos/webpack.config.js
@@ -2,5 +2,28 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-	
+	devtool: 'cheap-module-eval-source-map',
+	entry: [
+		'webpack-hot-middleware/client',
+		'/index'
+	],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/static/'
+	},
+	plugins: [
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+	],
+	module: {
+		loader: [
+			{
+				test: /\.js$/,
+				loader: ['babel'],
+				exclude:/node_modules/,
+				include: __dirname
+			}
+		]
+	}
 }
