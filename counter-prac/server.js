8eb53d7111fda36204e398fafd186a377ec4f65d
CalvinJin <jinchi2013us@gmail.com>
2016-07-25 13:17:49 -0400

    finish counter prac

--- counter-prac/server.js
+++ counter-prac/server.js
@@ -1,28 +1,23 @@
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.config');
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var config = require('./webpack.config')
 
-const app = new (require('express'))();
-const port = 3000;
+var app = new (require('express'))()
+var port = 3000
 
-const compiler = webpack(config);
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
 
-app.use(webpackDevMiddleware(compiler, {
-	noInfo: true, 
-	publicPath: config.output.publicPath
-}));
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
-app.use(webpackHotMiddleware(compiler));
-
-app.get("/", function(req, res){
-	res.sendFile(__dirname + '/index.html');
-});
-
-app.listen(port, function(error){
-	if(error){
-		console.error(error);
-	} else {
-		console.info('==> Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
-	}
-});
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
