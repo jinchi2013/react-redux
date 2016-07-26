ce36833b0ddb1e825dc0522711b71384a1a45e60
CalvinJin <jinchi2013us@gmail.com>
2016-07-26 13:56:31 -0400

    Add indexhtml and indexjs as the entry of the app

---
+++ todos-undo/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by chi on 7/26/16.
+ */
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Provider from 'react-redux';
+import { createStore } from 'redux';
+import App from './components/App.js';
+import todoApp from './reducers';
+
+const rootElement = document.getElementById('root');
+const store = createStore(todoApp);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
