ea8e2613bd913574c070fc9cc5891f899744ab28
Calvin <calvinjin.tmx@gmail.com>
2016-07-25 22:12:59 -0400

    fixed some syntax error

--- todos/components/Todo.js
+++ todos/components/Todo.js
@@ -1,16 +1,16 @@
 import React, { PropTypes } from 'react';
 
-const Todo = ( { onClick, completed, text} ) => {
-	<li
-		onClick={onClick}
+const Todo = ( { onClick, completed, text } ) => {
+	<li onClick={onClick}
 		style={{
 			textDecoration : completed ? 'line-through' : 'none'
-		}}> {text}
+		}}>
+		{text}
 	</li>
-}
+};
 
 Todo.propTypes = {
 	onClick: PropTypes.func.isRequired,
 	completed: PropTypes.bool.isRequired,
-	text.PropTypes.string.isRequired
+	text: PropTypes.string.isRequired
 };
