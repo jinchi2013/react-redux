96774cbac1cd5c5db9a751cc4608e5cd06a5933f
Calvin <calvinjin.tmx@gmail.com>
2016-07-25 22:12:11 -0400

    space changes

--- todos/actions/index.js
+++ todos/actions/index.js
@@ -5,18 +5,18 @@ export const addTodo = (text) => {
 		id: nextTodoId++,
 		text
 	}
-}
+};
 
 export const setVisibilityFilter = (filter) => {
 	return {
 		type: 'SET_VISIBILITY_FILTER',
 		filter
 	}
-}
+};
 
 export const toggleTodo = (id) => {
 	return {
 		type: 'TOGGLE_TODO',
 		id
 	}
-}
+};
