ccb9b4dccb11a777a7b0fabe6d060f57891f7d0d
Calvin <calvinjin.tmx@gmail.com>
2016-07-25 22:12:36 -0400

    Cap the VisibleTodoList

--- todos/components/App.js
+++ todos/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
-import VisibleTodolist from './containers/VisibleTodolist';
+import VisibleTodoList from '../containers/VisibleTodoList';
 
 const App = ()=> {
 	<div>
 		<AddTodo />
-		<VisibleTodolist />
+		<VisibleTodoList />
 		<Footer />
 	</div>
-}
+};
 
 export default App;
