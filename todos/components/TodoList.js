3b13f3c055e43575bf5802726b5fce2136137387
Calvin <calvinjin.tmx@gmail.com>
2016-07-25 22:13:13 -0400

    add comma

--- todos/components/TodoList.js
+++ todos/components/TodoList.js
@@ -11,7 +11,7 @@ const TodoList = ({ todos, onTodoClick }) => {
 			/>
 		})}
 	</ul>
-}
+};
 
 TodoList.propTypes = {
     todos: PropTypes.arrayOf(
@@ -22,6 +22,6 @@ TodoList.propTypes = {
     	}).isRequired
     ).isRequired,
     onTodoClick: PropTypes.func.isRequired
-}
+};
 
 export default TodoList;
