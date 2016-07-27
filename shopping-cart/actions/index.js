1e95cca644b34bc2cf1380892d43aa8495e50faa
CalvinJin <jinchi2013us@gmail.com>
2016-07-27 13:19:21 -0400

    done with actions: use shop.js and actiontypes constant

--- shopping-cart/actions/index.js
+++ shopping-cart/actions/index.js
@@ -1,3 +1,55 @@
 /**
  * Created by chi on 7/27/16.
  */
+import shop from '../api/shop';
+import * as types from '../constants/ActionTypes';
+
+const receiveProducts = (products) =>{
+    return {
+        type: types.RECEIVE_PRODUCTS,
+        products
+    };
+};
+
+export const getAllProducts = () => {
+    return(
+        (dispatch)=>{
+            shop.getProducts((products) =>{
+                dispatch(receiveProducts(products))
+            })
+        }
+    );
+};
+
+const addToCartUnsafe = (productId)=>{
+    return {
+        type: types.ADD_TO_CART,
+        productId
+    };
+};
+
+export const addToCart = (productId)=>{
+    return (dispatch, getState)=>{
+        if(getState().products.byId[productId].inventory > 0){
+            dispatch(addToCartUnsafe(productId))
+        }
+    }
+};
+
+export const checkout = (products)=>{
+    return (dispatch, getState) => {
+        const cart = getState().cart;
+
+        dispatch({
+            type: types.CHECKOUT_REQUEST
+        });
+        shop.buyProducts(products, () => {
+            dispatch({
+                type: types.CHECKOUT_SUCCESS,
+                cart
+            })
+        });
+        //Replace the line above with line below to rollback on failure:
+        //dispatch({ type: types.CHECKOUT_FAILURE, cart })
+    }
+};
