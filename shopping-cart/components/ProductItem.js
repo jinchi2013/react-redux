b11ec6143f80a815b5ea98132abb3d9a59577b5b
CalvinJin <jinchi2013us@gmail.com>
2016-07-27 11:20:50 -0400

    use Product finished ProductItem component

---
+++ shopping-cart/components/ProductItem.js
@@ -0,0 +1,42 @@
+/**
+ * Created by chi on 7/27/16.
+ */
+import React from 'react';
+import Product from './Product';
+
+class ProductItem extends React.Component{
+    render(){
+        const { product } = this.props;
+        return(
+            <div
+                style={
+                    { marginBottom: 20 }
+                }
+            >
+                <Product
+                    title={product.title}
+                    price={product.price}
+                />
+
+                <button
+                    onClick={this.props.onAddToCartClicked}
+                    disabled={product.inventory > 0 ? '' : 'disabled'}
+                >
+                    {product.inventory > 0 ? 'Add to cart' : 'Sold Out'}
+                </button>
+
+            </div>
+        );
+    }
+}
+
+ProductItem.propTypes = {
+    product: React.PropTypes.shape({
+        title: React.PropTypes.string.isRequired,
+        price: React.PropTypes.number.isRequired,
+        inventory: React.Proptypes.number.isRequired
+    }).isRequired,
+    onAddToCartClicked: React.PropTypes.func.isRequired
+};
+
+export default ProductItem;
