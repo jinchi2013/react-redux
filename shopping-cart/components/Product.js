def4cbe333a11d01d2e037446eb594834247c1f9
CalvinJin <jinchi2013us@gmail.com>
2016-07-27 10:59:39 -0400

    done with Product component

--- shopping-cart/components/Product.js
+++ shopping-cart/components/Product.js
@@ -1,3 +1,23 @@
 /**
  * Created by chi on 7/27/16.
  */
+import React from 'react';
+
+class Product extends React.Component {
+    render(){
+        const { price, quantity, title } = this.props;
+        return (
+            <div>
+                {title} - &#36;{price} {quantity ? `x ${quantity}` : null}
+            </div>
+        );
+    }
+}
+
+Product.propTypes = {
+    price : React.PropTypes.number,
+    quantity: React.PropTypes.number,
+    title: React.PropTypes.string
+};
+
+export default Product;
