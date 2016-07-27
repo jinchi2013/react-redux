6d4e6aa32e7775d75d7fd6226f16dc80ea0c1998
CalvinJin <jinchi2013us@gmail.com>
2016-07-27 15:38:39 -0400

    done with CartContainer

---
+++ shopping-cart/containers/ProductsContainer.js
@@ -0,0 +1,7 @@
+/**
+ * Created by chi on 7/27/16.
+ */
+import React from 'react';
+import { connect } from 'react-redux';
+import { addToCart } from '../actions';
+import { getVisibleProducts } from '../reducers/products';
