728bf364041b97ac8a1f5644ca8b34f99e36231e
Calvin Jin <Calvin.Jin.CFT@jcrew.com>
2016-08-01 16:36:27 -0400

    done with the actions

--- real-world/actions/index.js
+++ real-world/actions/index.js
@@ -53,3 +53,77 @@ export const loadRepo = (fullName, requiredFields=[]) => {
         return dispatch(fetchRepo(fullName));
     }
 };
+
+export const STARRED_REQUEST = "STARRED_REQUEST";
+export const STARRED_SUCCESS = "STARRED_SUCCESS";
+export const STARRED_FAILURE = "STARRED_FAILURE";
+
+// Fetches a page of starred repos by a particular user.
+// Relies on the custom API middleware defined in ../middleware/api.js.
+const fetchStarred = (login, nextPageUrl) => {
+    return {
+        login,
+        [CALL_API]: {
+            types: [ STARRED_REQUEST, STARRED_SUCCESS, STARRED_FAILURE ],
+            endpoint: nextPageUrl,
+            schema: Schemas.REPO_ARRAY
+        }
+    }
+};
+
+// Fetches a page of starred repos by a particular user.
+// Bails out if page is cached and user didn’t specifically request next page.
+// Relies on Redux Thunk middleware.
+export const loadStarred = (login, nextPage) => {
+    return (dispatch, getState) => {
+        const {
+            nextPageUrl = `users/${login}/starred`,
+            pageCount = 0
+        } = getState().pagination.starredByUser[login] || {};
+        if(pageCount > 0 && !nextPage) {
+            return null;
+        }
+
+        return dispatch( fetchStarred(login, nextPageUrl) );
+    }
+};
+export const STARGAZERS_REQUEST = "STARGAZERS_REQUEST";
+export const STARGAZERS_SUCCESS = "STARGAZERS_SUCCESS";
+export const STARGAZERS_FAILURE = "STARGAZERS_FAILURE";
+
+// Fetches a page of stargazers for a particular repo.
+// Relies on the custom API middleware defined in ../middleware/api.js.
+const fetchStargazers = (fullName, nextPageUrl) => {
+    return {
+        fullName,
+        [CALL_API]:{
+            types: [ STARGAZERS_REQUEST, STARGAZERS_SUCCESS, STARGAZERS_FAILURE ],
+            endpoint: nextPageUrl,
+            schema: Schemas.USER_ARRAY
+        }
+    };
+};
+// Fetches a page of stargazers for a particular repo.
+// Bails out if page is cached and user didn’t specifically request next page.
+// Relies on Redux Thunk middleware.
+export const loadStargazers = (fullName, nextPage) => {
+    return (dispatch, getState) => {
+        const {
+            nextPageUrl = `repos/${fullName}/stargazers`,
+            pageCount = 0
+        } = getState().pagination.stargazersByRepo[fullName] || {};
+
+        if (pageCount > 0 && !nextPage) {
+            return null;
+        }
+        return dispatch(fetchStargazers(fullName, nextPageUrl));
+    }
+};
+export const RESET_ERROR_MESSAGE = "RESET_ERROR_MESSAGE";
+
+//Resets the currently visible error message
+export const resetErrorMessage = ()=>{
+    return {
+        type: RESET_ERROR_MESSAGE;
+    }
+};
