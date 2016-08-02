b75bac59abedc9a484e0324861a7a726af4671d8
CalvinJin <jinchi2013us@gmail.com>
2016-08-02 16:52:20 -0400

    clustering the app in the shopping cart example

--- shopping-cart/clustering-app.js
+++ shopping-cart/clustering-app.js
@@ -1,3 +1,6 @@
+/**
+ * Created by chi on 8/2/16.
+ */
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
@@ -8,28 +11,28 @@ if(cluster.isMaster){
 
     Object.keys(cluster.workers).forEach( (id) => {
         console.log("I am running with ID: " + cluster.workers[id].process.pid);
-    } );
+} );
 
     cluster.on('fork', (worker)=>{
         console.log('Number of CPUs ' + numCPUs);
-        console.log('Worker %s spawned', worker.id);
-    });
+    console.log('Worker %s spawned', worker.id);
+});
     cluster.on('online', (worker)=>{
         console.log('worker %s online', worker.id);
-    });
+});
     cluster.on('listening', (worker, addr)=> {
         console.log('worker %s listening on %s:%d', worker.id, addr.address, addr.port);
-    });
+});
     cluster.on('disconnect', (worker)=>{
         console.log('worker %s disconnected', worker.id);
-    });
+});
     cluster.on('exit', (worker, code, signal)=>{
         console.error('worker %s died (%s)', worker.id, signal || code );
-        if( !worker.suicide ){
-            console.error('restarting worker');
-            cluster.fork();
-        }
-    });
+    if( !worker.suicide ){
+        console.error('restarting worker');
+        cluster.fork();
+    }
+});
 } else {
     require('./server');
 }
