8d888969c47f60ceb41410a2f1b3b0ff9284a612
Calvin Jin <jinchi2013us@gmail.com>
2016-12-05 10:51:34 -0500

    Add VisibleTodoList component

--- new-todos/components/App.js
+++ new-todos/components/App.js
@@ -1,16 +1,14 @@
-/**
- * Created by CalvinJ on 12/3/2016.
- */
 import React from 'react';
 import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
-//import VisibleTodoList from '../containers/VisibleTodoList';
+import VisibleTodoList from '../containers/VisibleTodoList';
 
 const App = () => {
     return (
         <div>
-            <AddTodo/>
-            <Footer/>
+            <AddTodo />
+            <VisibleTodoList />
+            <Footer />
         </div>
     );
 };
