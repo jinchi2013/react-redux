069e58de5a7a029fe51ed6f3799281c79004a304
Calvin Jin <jinchi2013us@gmail.com>
2016-12-05 10:55:09 -0500

    TodoList container and the list
    connect() method will pass the state as props to TodoList;
    Plus when the action is concerning with the reducer, we need
    mapDispatchToProps to use dispatch() to return the reducer method to
    change the state and return the new state

---
+++ new-todos/components/TodoList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Todo from './Todo';
+
+let TodoList = function({ todos, onTodoClick }) {
+    return(
+        <ul>
+            {
+                todos.map( (todo)=>{
+                    return(
+                        <Todo
+                            onClick={ ()=> onTodoClick(todo.id) }
+                            key={ todo.id }
+                            {...todo}
+                        />
+                    );
+                })
+            }
+        </ul>
+    );
+};
+
+TodoList.propTypes = {
+    todos: React.propTypes.arrayOf(
+        React.PropTypes.shape({
+            id: React.PropTypes.number.isRequired,
+            completed: React.PropTypes.bool.isRequired,
+            text: React.PropTypes.string.isRequired
+        })
+    ).isRequired,
+    onTodoClick: React.PropTypes.func.isRequired
+}
+
+export default TodoList;
---
+++ new-todos/containers/VisibleTodoList.js
@@ -0,0 +1,35 @@
+import { connect } from 'react-redux';
+import { toggleTodo } from '../actions';
+import TodoList from '../components/TodoList';
+
+let getVisibleTodos = function(toDos, filter) {
+    switch(filter) {
+        case 'SHOW_ALL':
+            return toDos;
+        case 'SHOW_COMPLETED':
+            return toDos.filter( (t)=>{ return t.completed; } );
+        case 'SHOW_ACTIVE':
+            return toDos.filter( (t)=>{ return !t.completed; } );
+    }
+};
+
+let mapStateToProps = function(state) {
+    return {
+        todos: getVisibleTodos(state.todos)
+    }
+};
+
+let mapDispatchToProps = function(dispatch) {
+    return {
+        onTodoClick: (id) => {
+            dispatch(toggleTodo(id))
+        }
+    }
+};
+
+let VisibleTodoList = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(TodoList);
+
+export default VisibleTodoList;
