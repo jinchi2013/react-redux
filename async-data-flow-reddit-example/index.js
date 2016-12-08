f7b6427d244f0e9a5317275b3594c5221f187cf6
Calvin Jin <jinchi2013us@gmail.com>
2016-12-08 14:42:18 -0500

    remove the double quote

--- async-data-flow-reddit-example/index.js
+++ async-data-flow-reddit-example/index.js
@@ -8,7 +8,7 @@ import configureStore from './store/configureStore';
 const store = configureStore();
 
 ReactDOM.render(
-    <Provider store="store">
+    <Provider store={store}>
         <App/>
     </Provider>,
     document.getElementById('root')
