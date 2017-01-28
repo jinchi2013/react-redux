c5eb0d7391223eac8a296449a67a20827e3ca4a6
Calvin Jin <jinchi2013us@gmail.com>
2017-01-27 23:24:33 -0500

    git api middleware
    Use normalizr to simplifiy the response of git API

--- monitor-git-repo/src/middleware/api.js
+++ monitor-git-repo/src/middleware/api.js
@@ -1,3 +1,4 @@
+"use strict";
 import { normalize, schema } from 'normalizr';
 import { camelizeKeys } from 'humps';
 
@@ -20,3 +21,91 @@ const API_ROOT = 'https://api.github.com';
 
 // Fetches an API response and normalizes the result JSON according to schma.
 // This makes every API response have the same shape, regardless of how nexted it was.
+const callApi = (endpoint, schema) => {
+    const fullUrl = (endpoint.indexOf(API_ROOT) === -1) ?
+        API_ROOT + endpoint : endpoint;
+    return fetch(fullUrl)
+        .then(response => response.json().then(json => {
+            if(!response.ok) {
+                return Promise.reject(json);
+            }
+
+            const camelizedJson = camelizeKeys(json);
+            const nextPageUrl = getNextPageUrl(response);
+
+            return Object.assign(
+                {},
+                normalize(camelizedJson, schema),
+                { nextPageUrl }
+            );
+        }))
+};
+
+const userSchema = new schema.Entity('user', {}, {
+    idAttribute: user => user.login.toLowerCase()
+});
+
+const repoSchema = new schema.Entity('repos', {
+    owner: userSchema
+}, {
+    idAttribute: repp => repo.fullName.toLowerCase()
+});
+
+// Schemas for Github API response
+export const Schemas = {
+    USER: userSchema,
+    USER_ARRAY: [userSchema],
+    REPO: repoSchema,
+    REPO_ARRAY: [repoSchema]
+};
+
+export const CALL_API = Symbol('Call API');
+
+// A Redux middleware that interprets actions with CALL_API info specified.
+// Performs the call and promises when such actions are dispatched.
+export default store => next => action => {
+    const callAPI = action[CALL_API];
+    if (typeof callAPI === 'undefined') {
+        return next(action)
+    }
+
+    let { endpoint } = callAPI;
+    const { schema, types } = callAPI;
+
+    if (typeof endpoint === 'function') {
+        endpoint = endpoint(store.getState())
+    }
+
+    if (typeof endpoint !== 'string') {
+        throw new Error('Specify a string endpoint URL.')
+    }
+    if (!schema) {
+        throw new Error('Specify one of the exported Schemas.')
+    }
+    if (!Array.isArray(types) || types.length !== 3) {
+        throw new Error('Expected an array of three action types.')
+    }
+    if (!types.every(type => typeof type === 'string')) {
+        throw new Error('Expected action types to be strings.')
+    }
+
+    const actionWith = data => {
+        const finalAction = Object.assign({}, action, data);
+        delete finalAction[CALL_API];
+        return finalAction
+    };
+
+    const [ requestType, successType, failureType ] = types;
+    next(actionWith({ type: requestType }));
+
+    return callApi(endpoint, schema).then(
+        response => next(actionWith({
+            response,
+            type: successType
+        })),
+        error => next(actionWith({
+            type: failureType,
+            error: error.message || 'Something bad happened'
+        }))
+    )
+}
