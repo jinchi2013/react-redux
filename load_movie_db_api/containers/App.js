99b7eeb8b39368b6e83e862f7db60fc19424b4db
Calvin Jin <jinchi2013us@gmail.com>
2017-03-04 01:06:40 -0500

    Finally this Api is working

--- load_movie_db_api/containers/App.js
+++ load_movie_db_api/containers/App.js
@@ -10,28 +10,50 @@ class App extends React.Component {
 		//this._clickHandler = this._clickHandler.bind(this);
 	}
 
+	componentDidMount() {
+		console.log('in the componentDidMount');
+		let {dispatch} = this.props;
+		dispatch( fetchMoviesIfNeeded() )
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if(nextProps.moviesList !== this.props.moviesList) {
+			let {dispatch} = nextProps
+			dispatch( fetchMoviesIfNeeded() )
+		}
+	}
+
 	render() {
-		let { isFetching, lastUpdated, moviesList } = this.props
-		let isEmpty = moviesList.length === 0
+		let { isPending, lastUpdated, movies } = this.props
+		let isEmpty = movies.movies.length === 0
+		console.log('this.props');
+		console.log(this.props);
 
 		return (
-				<div>Hello Movies</div>
+				<div>
+					<h1>Hello Movies</h1>
+					<ul>
+						{
+							movies.movies.map((movie, i) => {
+								return (
+										<li key={i}>
+											<h3>{movie.title}</h3>
+										</li>
+									)
+							})
+						}
+					</ul>
+				</div>
+
 			)
 	}
 }
 
 const mapStateToProps = (state) => {
 	let {movies} = state;
-	let {
-		isFetching,
-		lastUpdated,
-		movies: moviesList
-	} = movies.moviesList || { isFetching: true, movies: [] }
 
 	return {
-		isFetching,
-		lastUpdated,
-		moviesList
+		movies
 	}
 }
 
