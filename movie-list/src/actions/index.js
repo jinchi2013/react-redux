bd3f462a83b5bfbe0c559df514e0a509e3f23c9f
Calvin Jin <jinchi2013us@gmail.com>
2017-05-10 17:08:07 -0400

    async test with nock: have unknown issue

--- movie-list/src/actions/index.js
+++ movie-list/src/actions/index.js
@@ -1,8 +1,40 @@
+import { camelizeKeys } from 'humps'
 import {
-  REQUEST_TOP_RATED
+  REQUEST_TOP_RATED,
+  RECEIVE_TOP_RATED,
+  REQUEST_FAILED
 } from '../actionsConst'
 
-export const requestTopRated  = () => ({
+export const requestTopRated  = ()=>({
   type: REQUEST_TOP_RATED,
   isRequesting: true
 })
+
+export const receiveTopRated = (json)=>({
+  type: RECEIVE_TOP_RATED,
+  isRequesting: false,
+  requestFailed: false,
+  json: json
+})
+
+export const requestFailed = (err) => ({
+  type: REQUEST_FAILED,
+  requestFailed: true,
+  isRequesting: false,
+  err: err,
+})
+
+export const fetchTopRated = (testURL='', pageNumber=1) => (dispatch, getState) => {
+  dispatch(requestTopRated())
+  const hostName = testURL === ''? 'https://api.themoviedb.org' : testURL
+  return fetch(
+    `${hostName}/3/movie/top_rated?api_key=4bef8838c2fd078bd13d7127d8dedcd4&language=en-US?&page=${pageNumber}`
+  ).then(
+    response => response.json()
+  ).then(
+    json => {
+      const camelizeJson = camelizeKeys(json)
+      return dispatch(receiveTopRated(camelizeJson))
+    }, err => dispatch(requestFailed(err))
+  )
+}
--- movie-list/src/actions/index.spec.js
+++ movie-list/src/actions/index.spec.js
@@ -1,11 +1,72 @@
+import nock from 'nock'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
 import * as actions from './index'
 import * as types from '../actionsConst/'
 
-describe('movie actions', () => {
+// sync actions test
+describe('async : movie actions', () => {
   it('initiate the request to fetch data from top rated movie api', () => {
     expect(actions.requestTopRated()).toEqual({
       type: types.REQUEST_TOP_RATED,
       isRequesting: true
     })
+  })
+
+  it('handle receiveTopRated action, the fetch request sucess', ()=>{
+    const json = {data: 'json data'}
+    expect(actions.receiveTopRated(json)).toEqual({
+      type: types.RECEIVE_TOP_RATED,
+      json: json,
+      isRequesting: false,
+      requestFailed: false,
+    })
+  })
+
+  it('handle requestFailed action, some fetch request failed', ()=>{
+    const err = { msg: 'something is wrong!' };
+    expect(actions.requestFailed(err)).toEqual({
+      type: types.REQUEST_FAILED,
+      err: err,
+      requestFailed: true,
+      isRequesting: false,
+    })
+  })
+})
+
+// async actions
+const testURL = 'https://api.themoviedb.org'
+const middleware = [thunk]
+const mockStore = configureMockStore(middleware)
+
+describe('async: movie actions', ()=>{
+  afterEach(()=>{
+    nock.clearAll()
+  })
+
+  it('creates RECEIVE_TOP_RATED action when fetching movies has been done', ()=>{
+    nock(testURL)
+      .get('/3/movie/top_rated?api_key=4bef8838c2fd078bd13d7127d8dedcd4&language=en-US?&page=1')
+      .reply(200, {
+        results: ['movies array'],
+        page: 1,
+        totalPage: 10,
+        totalResults: 100
+      })
+
+    const initState = {}
+    const store = mockStore(initState)
+
+    store.dispatch(actions.fetchTopRated(testURL)).then(()=>{
+        expect(store.getActions()).toEqual([{
+          type: types.RECEIVE_TOP_RATED,
+          json: {
+            results: ['movies array'],
+            page: 1,
+            totalPage: 10,
+            totalResults: 100
+          }
+        }])
+      })
   })
 })
