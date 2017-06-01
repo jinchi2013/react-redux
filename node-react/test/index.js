f491abc2a806c93938c5891309c844acecc6d394
Calvin Jin <jinchi2013us@gmail.com>
2017-06-01 11:59:34 -0400

    fix the test

--- node-react/test/index.js
+++ node-react/test/index.js
@@ -1,11 +1,11 @@
 import http from 'http'
 import assert from 'assert'
 
-import '../lib/index.js'
+import '../server/index.js'
 
 describe('Example Node Server', () => {
   it('should return 200', done => {
-    http.get('http://127.0.0.1:1337', res => {
+    http.get('http://localhost:3001', res => {
       assert.equal(200, res.statusCode)
       done()
     })
