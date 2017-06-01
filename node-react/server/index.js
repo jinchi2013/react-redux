d407181b50f8e3e5baf8087a20e8e316a95f1b59
Calvin Jin <jinchi2013us@gmail.com>
2017-06-01 11:54:20 -0400

    refine the folder name

--- node-react/server/index.js
+++ node-react/server/index.js
@@ -3,6 +3,6 @@ import http from 'http'
 http.createServer( (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end('Hello World!\n')
-} ).listen(1337, '127.0.0.1', ()=>{
-  console.log('the server is running on 127.0.0.1:1337')
+} ).listen(3001, ()=>{
+  console.log('the server is running on http://localhost:3001/')
 })
