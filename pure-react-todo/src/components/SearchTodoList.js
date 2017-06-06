e952efa34c0df5134a1e2c616e0e2c1186ba339f
Calvin Jin <jinchi2013us@gmail.com>
2017-06-06 14:03:44 -0400

    remove useless console

--- pure-react-todo/src/components/SearchTodoList.js
+++ pure-react-todo/src/components/SearchTodoList.js
@@ -3,7 +3,6 @@ import React from 'react'
 const SearchTodoList = ({searchTodoList}) => {
   function handleOnChange(e) {
     searchTodoList(e.target.value)
-    console.log('typeof e.target.value', typeof e.target.value)
   }
 
   return (
